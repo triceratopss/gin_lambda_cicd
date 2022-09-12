120dc60789a862373646dc5ffe80b112eadd259b
triceratopss <toshiki.12323@gmail.com>
2022-09-12 22:37:34 +0900

    modify

--- src/handler/main.go
+++ src/handler/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,7 +18,7 @@ func init() {
     r := gin.Default()
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
-            "message": "CICD Success!",
+            "message": "CICD Check!",
         })
     })
     r.GET("/hello", func(c *gin.Context) {
